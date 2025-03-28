d8316684569d656f68b60ffb348f8ec281a361e1
Alexander Samra <125580889+alexsamra@users.noreply.github.com>
2025-03-28 16:19:17 -0400

    Screen rotation cheating updated

--- app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.kt
@@ -10,6 +10,7 @@ import com.bignerdranch.android.geoquiz.databinding.ActivityCheatBinding
 const val EXTRA_ANSWER_SHOWN = "com.bignerdranch.android.geoquiz.answer_shown"
 private const val EXTRA_ANSWER_IS_TRUE =
     "com.bignerdranch.android.geoquiz.answer_is_true"
+private const val HAS_CHEATED_KEY = "has_cheated"
 
 class CheatActivity : AppCompatActivity() {
 
@@ -17,19 +18,40 @@ class CheatActivity : AppCompatActivity() {
 
     private var answerIsTrue = false
 
+    private var hasCheated = false
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean(HAS_CHEATED_KEY, hasCheated)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCheatBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
+        //Based on saved instance state, check if the user has cheated through hasCheated variable
+        //If null set to false
+        hasCheated = savedInstanceState?.getBoolean(HAS_CHEATED_KEY, false) ?: false
+
+        //Move answer text outside of onClickListener
+        val answerText = when {
+            answerIsTrue -> R.string.true_button
+            else -> R.string.false_button
+        }
+
+        //If user has cheated run setAnswerShownResult
+        //Text is updated properly
+        if (hasCheated) {
+            setAnswerShownResult(true)
+            binding.answerTextView.setText(answerText)
+        }
 
         binding.showAnswerButton.setOnClickListener {
-            val answerText = when {
-                answerIsTrue -> R.string.true_button
-                else -> R.string.false_button
-            }
             binding.answerTextView.setText(answerText)
+            //set has cheated to true to save after screen rotation
+            hasCheated = true
             setAnswerShownResult(true)
         }
     }
